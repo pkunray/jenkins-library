41495b71d8c128b5afc646583068250365fdcd97
pkunray <pkunray@gmail.com>
2024-10-10 18:14:19 +0200

    fix: Correct metadata file of CTP

--- vars/detectCTPExecuteScan.groovy
+++ vars/detectCTPExecuteScan.groovy
@@ -5,7 +5,7 @@ import groovy.transform.Field
 import static com.sap.piper.Prerequisites.checkScript
 
 @Field String STEP_NAME = getClass().getName()
-@Field String METADATA_FILE = 'metadata/detectExecuteScan.yaml'
+@Field String METADATA_FILE = 'metadata/detectCTPExecuteScan.yaml'
 
 void call(Map parameters = [:]) {
     final script = checkScript(this, parameters) ?: this
