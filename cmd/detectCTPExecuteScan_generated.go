31081e2f9d003c1d2cede952b3a37291444a1625
Kun Pang <kun.pang@sap.com>
2024-10-15 10:46:35 +0200

    chore: Remove output reports for detectCTP

--- cmd/detectCTPExecuteScan_generated.go
+++ cmd/detectCTPExecuteScan_generated.go
@@ -6,18 +6,14 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"reflect"
-	"strings"
 	"time"
 
 	"github.com/SAP/jenkins-library/pkg/config"
-	"github.com/SAP/jenkins-library/pkg/gcs"
 	"github.com/SAP/jenkins-library/pkg/log"
 	"github.com/SAP/jenkins-library/pkg/piperenv"
 	"github.com/SAP/jenkins-library/pkg/splunk"
 	"github.com/SAP/jenkins-library/pkg/telemetry"
 	"github.com/SAP/jenkins-library/pkg/validation"
-	"github.com/bmatcuk/doublestar"
 	"github.com/spf13/cobra"
 )
 
@@ -128,51 +124,6 @@ func (i *detectCTPExecuteScanInflux) persist(path, resourceName string) {
 	}
 }
 
-type detectCTPExecuteScanReports struct {
-}
-
-func (p *detectCTPExecuteScanReports) persist(stepConfig detectCTPExecuteScanOptions, gcpJsonKeyFilePath string, gcsBucketId string, gcsFolderPath string, gcsSubFolder string) {
-	if gcsBucketId == "" {
-		log.Entry().Info("persisting reports to GCS is disabled, because gcsBucketId is empty")
-		return
-	}
-	log.Entry().Info("Uploading reports to Google Cloud Storage...")
-	content := []gcs.ReportOutputParam{
-		{FilePattern: "**/*BlackDuck_RiskReport.pdf", ParamRef: "", StepResultType: "blackduck-ip"},
-		{FilePattern: "**/blackduck-ip.json", ParamRef: "", StepResultType: "blackduck-ip"},
-		{FilePattern: "**/toolrun_detectExecute_*.json", ParamRef: "", StepResultType: "blackduck-ip"},
-		{FilePattern: "**/piper_detect_policy_violation_report.html", ParamRef: "", StepResultType: "blackduck-ip"},
-		{FilePattern: "**/*BlackDuck_RiskReport.pdf", ParamRef: "", StepResultType: "blackduck-security"},
-		{FilePattern: "**/detectExecuteScan_policy_*.json", ParamRef: "", StepResultType: "blackduck-security"},
-		{FilePattern: "**/piper_detect_vulnerability_report.html", ParamRef: "", StepResultType: "blackduck-security"},
-		{FilePattern: "**/toolrun_detectExecute_*.json", ParamRef: "", StepResultType: "blackduck-security"},
-		{FilePattern: "**/piper_detect_vulnerability.sarif", ParamRef: "", StepResultType: "blackduck-security"},
-		{FilePattern: "**/piper_hub_detect_sbom.xml", ParamRef: "", StepResultType: "blackduck-security"},
-	}
-	envVars := []gcs.EnvVar{
-		{Name: "GOOGLE_APPLICATION_CREDENTIALS", Value: gcpJsonKeyFilePath, Modified: false},
-	}
-	gcsClient, err := gcs.NewClient(gcs.WithEnvVars(envVars))
-	if err != nil {
-		log.Entry().Errorf("creation of GCS client failed: %v", err)
-		return
-	}
-	defer gcsClient.Close()
-	structVal := reflect.ValueOf(&stepConfig).Elem()
-	inputParameters := map[string]string{}
-	for i := 0; i < structVal.NumField(); i++ {
-		field := structVal.Type().Field(i)
-		if field.Type.String() == "string" {
-			paramName := strings.Split(field.Tag.Get("json"), ",")
-			paramValue, _ := structVal.Field(i).Interface().(string)
-			inputParameters[paramName[0]] = paramValue
-		}
-	}
-	if err := gcs.PersistReportsToGCS(gcsClient, content, inputParameters, gcsFolderPath, gcsBucketId, gcsSubFolder, doublestar.Glob, os.Stat); err != nil {
-		log.Entry().Errorf("failed to persist reports: %v", err)
-	}
-}
-
 // DetectCTPExecuteScanCommand Executes Synopsys Detect scan
 func DetectCTPExecuteScanCommand() *cobra.Command {
 	const STEP_NAME = "detectCTPExecuteScan"
@@ -181,7 +132,6 @@ func DetectCTPExecuteScanCommand() *cobra.Command {
 	var stepConfig detectCTPExecuteScanOptions
 	var startTime time.Time
 	var influx detectCTPExecuteScanInflux
-	var reports detectCTPExecuteScanReports
 	var logCollector *log.CollectorHook
 	var splunkClient *splunk.Splunk
 	telemetryClient := &telemetry.Telemetry{}
@@ -243,7 +193,6 @@ Please configure your BlackDuck server Url using the serverUrl parameter and the
 			stepTelemetryData.ErrorCode = "1"
 			handler := func() {
 				influx.persist(GeneralConfig.EnvRootPath, "influx")
-				reports.persist(stepConfig, GeneralConfig.GCPJsonKeyFilePath, GeneralConfig.GCSBucketId, GeneralConfig.GCSFolderPath, GeneralConfig.GCSSubFolder)
 				config.RemoveVaultSecretFiles()
 				stepTelemetryData.Duration = fmt.Sprintf("%v", time.Since(startTime).Milliseconds())
 				stepTelemetryData.ErrorCategory = log.GetErrorCategory().String()
@@ -957,22 +906,6 @@ func detectCTPExecuteScanMetadata() config.StepData {
 						Parameters: []map[string]interface{}{
 							{"name": "step_data", "fields": []map[string]string{{"name": "detect"}}},
 							{"name": "detect_data", "fields": []map[string]string{{"name": "vulnerabilities"}, {"name": "major_vulnerabilities"}, {"name": "minor_vulnerabilities"}, {"name": "components"}, {"name": "policy_violations"}}},
-						},
-					},
-					{
-						Name: "reports",
-						Type: "reports",
-						Parameters: []map[string]interface{}{
-							{"filePattern": "**/*BlackDuck_RiskReport.pdf", "type": "blackduck-ip"},
-							{"filePattern": "**/blackduck-ip.json", "type": "blackduck-ip"},
-							{"filePattern": "**/toolrun_detectExecute_*.json", "type": "blackduck-ip"},
-							{"filePattern": "**/piper_detect_policy_violation_report.html", "type": "blackduck-ip"},
-							{"filePattern": "**/*BlackDuck_RiskReport.pdf", "type": "blackduck-security"},
-							{"filePattern": "**/detectExecuteScan_policy_*.json", "type": "blackduck-security"},
-							{"filePattern": "**/piper_detect_vulnerability_report.html", "type": "blackduck-security"},
-							{"filePattern": "**/toolrun_detectExecute_*.json", "type": "blackduck-security"},
-							{"filePattern": "**/piper_detect_vulnerability.sarif", "type": "blackduck-security"},
-							{"filePattern": "**/piper_hub_detect_sbom.xml", "type": "blackduck-security"},
 						},
 					},
 				},
